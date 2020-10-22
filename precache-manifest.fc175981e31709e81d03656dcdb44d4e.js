d3e04716ba85424395ed381da5e0021cb4bd634f
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-10-21 19:52:04 -0500

    Updates

---
+++ precache-manifest.fc175981e31709e81d03656dcdb44d4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47b08c20644a0dcdd8f6282fae830cca",
+    "url": "/Amazon-clone-app/index.html"
+  },
+  {
+    "revision": "d2cfaa5852a5d36897bb",
+    "url": "/Amazon-clone-app/static/css/main.c9522ae3.chunk.css"
+  },
+  {
+    "revision": "18cb58d4fc235099b8b5",
+    "url": "/Amazon-clone-app/static/js/2.48098d92.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Amazon-clone-app/static/js/2.48098d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cfaa5852a5d36897bb",
+    "url": "/Amazon-clone-app/static/js/main.26351dad.chunk.js"
+  },
+  {
+    "revision": "86383d1ccc9af870da97",
+    "url": "/Amazon-clone-app/static/js/runtime-main.cdf46066.js"
+  }
+]);
